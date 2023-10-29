94f99e5da5e31e62e3c98ac1523df4df0a3556b5
arnav108276 <143831459+arnav108276@users.noreply.github.com>
2023-10-29 14:53:59 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE password("
+                   "id INT AUTO_INCREMENT PRIMARY KEY,"
+                   "Username VARCHAR(255) NOT NULL,"
+                   "Password VARCHAR(255) NOT NULL,"
+                   "website VARCHAR(255) NOT NULL"
+                   ")
